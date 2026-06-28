48be7e65fbf88e11eee5413b3df3cfbc6986bb4d
Алексей Зорин <alex-4-17@yandex.ru>
2026-06-28 09:21:34 +0400

    style: CS Fixer для ServiceAliasResolverMutationTest

--- tests/Unit/ServiceAliasResolverMutationTest.php
+++ tests/Unit/ServiceAliasResolverMutationTest.php
@@ -7,6 +7,7 @@ namespace CloudCastle\DI\Tests\Unit;
 use CloudCastle\DI\ServiceAliasResolver;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 
 #[CoversClass(ServiceAliasResolver::class)]
 final class ServiceAliasResolverMutationTest extends TestCase
@@ -34,7 +35,7 @@ final class ServiceAliasResolverMutationTest extends TestCase
     public function testResolveThrowsOnThreeStepAliasCycle(): void
     {
         $resolver = new ServiceAliasResolver();
-        $aliases = (new \ReflectionClass(ServiceAliasResolver::class))->getProperty('aliases');
+        $aliases = (new ReflectionClass(ServiceAliasResolver::class))->getProperty('aliases');
         $aliases->setValue($resolver, ['first' => 'second', 'second' => 'third', 'third' => 'first']);
 
         $this->expectException(\CloudCastle\DI\Exception\ContainerException::class);
